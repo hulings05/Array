94c89390f3c7c1d96015df63598c40c1b0ef398b
hulings05 <97485236+hulings05@users.noreply.github.com>
2022-04-27 13:57:45 -0400

    Add files via upload

---
+++ Car Array.cs
@@ -0,0 +1,162 @@
+using System;
+
+public abstract class Car
+{
+ protected virtual int color {get{return 0;}}
+ protected virtual int make{get{return 0;}}
+
+ public void Printcolor()
+ {
+     Console.WriteLine(
+         string.Format(" -  -Color is {0}",
+         this.color));
+ }
+ public void printMake()
+ {
+     Console.WriteLine(
+         string.Format(" - Make is {0}",
+         this.make));
+ }
+}
+
+public class Insert : Car
+{
+    protected override int color { get{ return 1;}}
+    protected override int make { get{return 1;}}
+    public virtual void color()
+    {
+        Console.WriteLine(String.Format(" -Color is black {0}", this.color));
+    }
+    public virtual void make()
+    {
+        Console.WriteLine(String.Format(" -Make is Chevy {0}", this.make));
+    }
+    public void carnoise()
+    {
+        Console.WriteLine("vroom vroom");
+    }
+    public void Speed()
+    {
+        Console.WriteLine(" - Max speed is 160");
+    }
+    public void Doors()
+    {
+        Console.WriteLine(" - 4 doors");
+    }
+    public void MPG()
+    {
+        Console.WriteLine(" - MPG is 30");
+    }
+}
+}
+
+public class Truck : Insert
+{
+    protected override int color { get { return 3; } }
+    protected override int make { get{return 5;}}
+    public virtual void color()  
+    { 
+        Console.WriteLine(String.Format(" -Color is red {0}", this.color));         
+    } 
+    public virtual void make()  
+    { 
+        Console.WriteLine(String.Format(" -Make is Dodge {0}", this.make));         
+    }
+    public void trucknoise()
+    {
+        Console.WriteLine("purr purr");
+    }
+    public void Speed()
+    {
+        Console.WriteLine(" - Max speed is 120");
+    }
+    public void Doors()
+    {
+        Console.WriteLine(" - 2 doors");
+    }
+    public void MPG()
+    {
+        Console.WriteLine(" - MPG is 14");
+    }
+}
+public class Van : Insert
+{
+    protected override int color { return 4; } }
+    protected override int make {get{return 2;}}
+    public virtual void color()   
+    { 
+        Console.WriteLine(String.Format(" - Color is blue {0}", this.color));         
+    } 
+    public virtual void make()  
+    { 
+        Console.WriteLine(String.Format(" - Make is Ford {0}", this.make));         
+    }
+    public void vannoise()
+    {
+        Console.WriteLine("zoom zoom");
+    }
+    public void Speed()
+    {
+        Console.WriteLine(" - Max speed is 140");
+    }
+    public void Doors()
+    {
+        Console.WriteLine(" - 2 doors");
+    }
+    public void MPG()
+    {
+        Console.WriteLine(" - MPG is 20");
+    }
+}
+
+
+public class Valet
+{
+    static void Main(string[] args)
+    {
+    Car car1 = new Car();
+    Console.WriteLine("Your car has arrived");
+        car1.color();
+        car1.make();
+        car1.carnoise();
+        car1.Speed();
+        car1.Doors();
+        car1.MPG();
+         int[] Car = {1,3,5};
+    int sum1 = 0;
+    foreach (int item in Car {
+        sum1 += item;
+        }
+        Console.WriteLine("Car engine widths are: {0}",sum1);
+
+    Truck car2 = new Truck();
+    Console.WriteLine("Your truck has arrived");
+        car2.color();
+        car2.make();
+        car2.trucknoise();
+        car2.Speed();
+        car2.Doors();
+        car2.MPG();
+         int[] Truck = {2,4,7};
+    int sum2 = 0;
+    foreach (int item in Truck) {
+        sum2 += item;
+        }
+        Console.WriteLine("Truck engine widths are: {0}",sum2);
+
+    Van car3 = new Van();
+    Console.WriteLine("Your van has arrived");
+        car3.color();
+        car3.make();
+        car3.vannoise();
+        car3.Speed();
+        car3.Doors();
+        car3.MPG();
+        int[] Van = {3,8,9};
+    int sum3 = 0;
+    foreach (int item in Van) {
+        sum3 += item;
+        }
+        Console.WriteLine("Van engine widths are: {0}",sum3);
+    }
+}
